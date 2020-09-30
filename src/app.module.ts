1164050c1dec1402e906719e704f7e53c188865d
Hoang Trinh <hoang.trinhj@gmail.com>
2020-09-30 11:34:43 +0700

    Initialize Project

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [ConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
