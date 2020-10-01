5fe031eab582054c23af469dc897cf134ab786e5
Hoang Trinh <hoang.trinhj@gmail.com>
2020-10-01 18:13:04 +0700

    Connect Redis Store

---
+++ src/cache/redisCache.module.ts
@@ -0,0 +1,18 @@
+import { Module, CacheModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as redisStore from 'cache-manager-redis-store';
+
+@Module({
+  imports: [
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get('REDIS_HOST'),
+        port: configService.get('REDIS_PORT'),
+      }),
+    }),
+  ],
+})
+export class RedisCacheModule {}
